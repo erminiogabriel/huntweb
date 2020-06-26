ef9a9e70cf5076807b759daf1eda0c743cf260c3
Gabriel Machado <gabriel@movitech.com.br>
2020-06-26 10:15:25 -0300

    Adicionado a interface de visualizacao de produtos de forma detalhada

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Header from "./components/Header";
 import "./styles.css";
-import api from "./services/api";
-import Main from './pages/main';
+import Routes from "./routes"
 
 const App = () => {
   return ( 
     <div className="App">
       <Header/>
-      <Main/>
+      <Routes/>
     </div>
   );
 }
